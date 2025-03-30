791239de7313b344af249ff4377e27848b48ce5e
LadjamatliAbusood <ladjamatli5@gmail.com>
2025-03-30 11:16:49 +0800

    updating footer

--- src/components/FooterSection.vue
+++ src/components/FooterSection.vue
@@ -1,13 +1,12 @@
 <template>
-  <footer class="mt-3 border z-10 border-t-[#33353F] border-l-transparent border-r-transparent">
-    <div class="container p-5 flex justify-between">
-        <div class=" text-3xl font-bold"></div>
-        <p class="text-slate-600">All right reserved. 2025</p>
+  <footer
+    class="mt-3 border z-10 border-t-[#33353F] border-l-transparent border-r-transparent"
+  >
+    <div class="container p-5 flex justify-center">
+      <div class="text-3xl font-bold"></div>
+      <p class="text-slate-600">All right reserved. 2025</p>
     </div>
-
   </footer>
 </template>
 
-<script>
-
-</script>
+<script></script>
